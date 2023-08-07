087710348d2b78a7fdf2a78d671785aeafaf1d95
pralim <limbuprajil123@gmail.com>
2023-08-08 02:57:48 +0300

    form for proposing names and reading them

--- src/components/contest.tsx
+++ src/components/contest.tsx
@@ -18,12 +18,19 @@ const Contest = ({initialContest, onContestListClick}) => {
         onContestListClick()
     }
 
+    const handleNewNameSubmit = (event) => {
+        event.preventDefault()
+        const newNameInput = event.target.newName
+        console.log(newNameInput.value)
+    }
+
     return (
         <>
             <Header message={contest.contestName} />
             <div className="contest">
                 <div className="title">Content description</div>
                 <div className="description">{contest.description}</div>
+
                 <div className="title">Proposed Names</div>
                 <div className="body">
                     {contest.names?.length > 0 ? (
@@ -36,6 +43,15 @@ const Contest = ({initialContest, onContestListClick}) => {
                         <div>No names proposed yet</div>
                     )}
                 </div>
+
+                <div className="title">Propose a New Name</div>
+                <div className="body">
+                    <form>
+                        <input type="text" name="newName" placeholder="New Name Here ..." />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+
                 <a href="/" className="link" onClick={handleClickContestList}>Contest List</a>
             </div>
         </>
